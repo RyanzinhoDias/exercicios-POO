1c3315fc127049c097ed69aeac5e37e508a8084a
RyanzinhoDias <ryanddsn0@gmail.com>
2024-09-24 14:54:30 -0300

    atualização

--- praticaheranca/src/main/java/org/example/Main.java
+++ praticaheranca/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Aluno meuAluno = new Aluno("Ryan", 20, "Masculino", "POO");
-        Monitor meuMonitor = new Monitor("Ryan", 21, "Masculino", 8);
+        Aluno meuAluno = new Aluno("Nome", 25, "Masculino", "POO");
+        Monitor meuMonitor = new Monitor("nome", 34, "Masculino", 8);
 
         System.out.printf("Nome: %s\nIdade: %d\nSexo: %s\nTurma: %s\n", meuAluno.getNome(), meuAluno.getIdade(), meuAluno.getSexo(), meuAluno.getTurma());
         System.out.printf("\nNome: %s\nIdade: %d\nSexo: %s\nTurma: %s", meuMonitor.getNome(), meuMonitor.getIdade(), meuMonitor.getSexo(), meuMonitor.getHorasTrabalhadas());
